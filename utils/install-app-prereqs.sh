9cab019f2c23b0c67ac28a9a20527d3b29000e9c
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2020-03-27 20:39:10 +0100

    Docker part 1: run automated tests in docker

---
+++ utils/install-app-prereqs.sh
@@ -0,0 +1,12 @@
+function ensure_success {
+	exit_status=$?
+	if [ $exit_status -ne 0 ]; then
+		echo "... command exited with code $exit_status"
+		exit $exit_status
+	fi
+}
+
+echo "Installing app prerequisites..."
+apt-get -qqy update
+apt-get -qqy install libgdiplus
+ensure_success
